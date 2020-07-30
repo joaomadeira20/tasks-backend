ffaac72549cf54015e33b61312ffd93045f26173
thantarus123 <45951200+thantarus123@users.noreply.github.com>
2020-07-30 19:22:16 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const bd = require('./config/db')
+const consign = require('consign')
+
+consign()
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
+app.db = bd
+
+app.listen(3000, () => {
+    console.log('back top')
+})
